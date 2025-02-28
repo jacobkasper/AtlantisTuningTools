86522d24587d80fbab94efdacd754a0cf299f95d
jacobmk <jacob.matthew.kasper@hafogvatn.is>
2025-02-28 10:35:17 +0000

    updated CompareBiologicalPrms.R for comparing within the same model

--- R/CompareBiologicalPrms.r
+++ R/CompareBiologicalPrms.r
@@ -10,21 +10,22 @@
 #' @examples
 #' ##CompBioPrm("E:/Atlantis/input/atlantis6702/Biological_6702_2025_02_07_02.prm",
 #' "E:/Atlantis/NoBa_params/nordic_biology.prm",
-#' "WMW", "MWH")
+#' "WMW", "MWH", 1)
 #' my_function(10)
 #' @export
-CompBioPrm <- function(file1, file2, file1group, file2group){
+CompBioPrm <- function(file1, file2, file1group, file2group, diffmodel){
     f1groupcode <- file1group
     f2groupcode <- file2group
-    f1nboxes <- 53 ##number of boxes in model 1
-    file1_data <- tidyr::as_tibble(extract_parameters(file1, f1groupcode))
-    file2_data <- tidyr::as_tibble(extract_parameters(file2, f2groupcode))
-    file2_data$Parameter <- sub(f2groupcode, f1groupcode, file2_data$Parameter)##if group codes differ
-    out <- tidyr::as_tibble(dplyr::full_join(file1_data, file2_data, by = "Parameter",
-                                             suffix = c(paste0("_", f1groupcode), paste0("_", f2groupcode)))) |>
-        dplyr::filter(get(paste0("Value_", f1groupcode)) != get(paste0("Value_", f2groupcode)) |
-                      is.na(get(paste0("Value_", f1groupcode))) |
-                      is.na(get(paste0("Value_", f2groupcode))))
+    f1nboxes <- 53
+    file1_data <- dplyr::tibble(extract_parameters(file1, f1groupcode))
+    file2_data <- dplyr::tibble(extract_parameters(file2, f2groupcode))
+    if(diffmodel==1) {file2_data$Parameter <- sub(f2groupcode, f1groupcode, file2_data$Parameter)}
+    out <- tidyr::as_tibble(
+                      dplyr::full_join(file1_data, file2_data, by = "Parameter",
+                                       suffix = c(paste0("_m1_", f1groupcode), paste0("_m2_", f2groupcode)))) |>
+        dplyr::filter(get(paste0("Value_m1_", f1groupcode)) != get(paste0("Value_m2_", f2groupcode)) |
+                      is.na(get(paste0("Value_m1_", f1groupcode))) |
+                      is.na(get(paste0("Value_m2_", f2groupcode))))
     return(out)
 }
 
--- R/MigSpawn.R
+++ R/MigSpawn.R
@@ -7,39 +7,31 @@
 #' @export
 MigSpawnRecTime <- function(bfile, speciescode, mfile, matstage){
     bfile <- readLines(bfile)
-    prmget <- stringr::str_c("Recruit_Period_", speciescode, "|", speciescode, "_Recruit_Time|", speciescode, "_spawn_period|", speciescode, "_Time_Spawn")
+    prmget <- stringr::str_c("Recruit_Period_", speciescode,
+                             "|", speciescode, "_Recruit_Time|",
+                             speciescode, "_spawn_period|",
+                             speciescode, "_Time_Spawn")
     matching_lines <- stringr::str_subset(bfile, prmget)
-    result_table <- dplyr::tibble(
-        Parameter = stringr::str_extract(matching_lines, "^\\S+"),
-        Value = as.numeric(stringr::str_extract(matching_lines, "\\d+"))
-    )
+    repdat <-
+        dplyr::tibble(Parameter = stringr::str_extract(matching_lines,
+        "^\\S+"), Value = as.numeric(stringr::str_extract(matching_lines, "\\d+")))    |>
+        dplyr::mutate(Event = c('Spawn_Day', 'Recruit_End', 'Recruit_End', 'Recruit_End')) |>
+        dplyr::group_by(Event) |>
+        dplyr::summarise(day = sum(Value))    |>
+        dplyr::mutate(day=ifelse(Event=='Recruit_End', sum(day), day))    |>
+        dplyr::mutate(day2=ifelse(day>365, day-365, day))
     migdat <-
-        readr::read_csv(mfile) |>
-    dplyr::filter(GroupCode == speciescode, StartStage == matstage)    |>
-        tidyr::pivot_longer(col = -c(GroupCode),
-                            names_to = 'Parameter', values_to = 'Value') |>
-        dplyr::filter(Parameter %in% c('StartTofY', 'EndTofY', 'Leave_Period','Return_Period'))|>
-        dplyr::select(-GroupCode)
-    out <- dplyr::bind_rows(result_table, migdat) |>
-            dplyr::mutate(order = c(1, 2, 5, 6, 7, 3, 8, 4)) |>
-        dplyr::arrange(order) |>
-                                        # Compute the sums separately inside a lambda function
-        (\(df) {
-            spawn_sum <- df |> dplyr::slice(1:2) |> dplyr::pull(Value) |> sum()
-            return_sum <- df |> dplyr::slice(3:4) |> dplyr::pull(Value) |> sum()
-            recruit_sum <- df |> dplyr::slice(5:6) |> dplyr::pull(Value) |> sum()
-            leave_sum <- df |> dplyr::slice(7:8) |> dplyr::pull(Value) |> sum()
-            df |>
-                dplyr::add_row(Parameter = "Spawn_End", Value = spawn_sum, order = 2.5) |>
-                dplyr::add_row(Parameter = "Return_End", Value = return_sum, order = 4.5) |>
-                dplyr::add_row(Parameter = "Recruit_End", Value = recruit_sum, order = 5.5) |>
-                dplyr::add_row(Parameter = "Leave_End", Value = leave_sum, order = 6.5)
-        })() |>
-         dplyr::arrange(order) |>
-         dplyr::mutate(order = dplyr::row_number()) |>
-         dplyr::filter(order %in% c(3, 6, 8, 10)) |>
-         dplyr::mutate(domain = c(0, 1, 1, 0)) |>
-         dplyr::select(- order)
+        dplyr::select(dplyr::filter(tidyr::pivot_longer(dplyr::filter(readr::read_csv(mfile),
+        GroupCode == speciescode, StartStage == matstage), col = -c(GroupCode),
+        names_to = "Parameter", values_to = "Value"), Parameter %in%
+        c("StartTofY", "EndTofY", "Leave_Period", "Return_Period")),
+        -GroupCode) |>
+    dplyr::mutate(Event = c("MigOut", "MigIn", "MigOut", "MigIn")) |>
+    dplyr::group_by(Event) |>
+        dplyr::summarise(day = sum(Value)) |>
+        dplyr::mutate(day2=ifelse(day>365, day-365, day))
+    out <-     dplyr::bind_rows(repdat, migdat) |>
+        dplyr::arrange(day2)
     return(out)
 }
 
